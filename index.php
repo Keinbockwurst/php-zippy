108f6178bfa129f20d1af5247fd43a77667916c2
Keinbockwurst <sboguth@web.de>
2016-10-20 09:55:56 +0200

    added Uploader class

--- index.php
+++ index.php
@@ -26,50 +26,9 @@ if (isset($_POST['dozip'])) {
   Zipper::zipDir($zippath, $zipfile);
 }
 if (isset($_POST['upload'])) {
-    $directory = "upload/";
-
-    if (strlen(pathinfo($_FILES['uploaded']['name'], PATHINFO_FILENAME)) == 0) {
-      $GLOBALS['status'] = array('error' => 'Fehler: Sie müssen eine Datei zum hochladen auswählen!');
-    }
-    else {
-      //checking if dir upload exists, if not create it.
-      if (!file_exists($directory)) {
-          mkdir($directory, 0755);
-      }
-      //moving the uploaded files to the upload-dir
-      $upload_folder = 'upload/'; //Das Upload-Verzeichnis
-      $filename = pathinfo($_FILES['uploaded']['name'], PATHINFO_FILENAME);
-      $extension = strtolower(pathinfo($_FILES['uploaded']['name'], PATHINFO_EXTENSION));
+    Uploader::doUpload();
 
 
-      //Überprüfung der Dateiendung
-      $allowed_extensions = array('rar', 'zip', 'gz');
-      if(!in_array($extension, $allowed_extensions)) {
-      	die("<p><b><font color='red' face='arial'>Fehler: </font><font face='arial'>Ungueltige Dateiendung. Nur .rar, .zip und .gz sind erlaubt.</font></b></p>");
-      }
-
-      //Überprüfung der Dateigröße
-      $max_size = 10000*1024; //10 MB
-      if($_FILES['uploaded']['size'] > $max_size) {
-      	die("<p><b><font color='red' face='arial'>Fehler: </font> <font face='arial'>Bitte keine Dateien größer 10 MB hochladen.</font></b></p>");
-      }
-
-      //Pfad zum Upload
-      $new_path = $upload_folder.$filename.'.'.$extension;
-
-      //Neuer Dateiname falls die Datei bereits existiert
-      if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
-      	$id = 1;
-      	do {
-      		$new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
-      		$id++;
-      	} while(file_exists($new_path));
-      }
-
-      //Alles okay, verschiebe Datei an neuen Pfad
-      move_uploaded_file($_FILES['uploaded']['tmp_name'], $new_path);
-      $GLOBALS['status'] = array('success' => 'Datei erfolgreich hochgeladen.');
-  }
 }
 $timeend = microtime(TRUE);
 $time = $timeend - $timestart;
@@ -77,6 +36,58 @@ $timestring = (string)$time;
 /**
  * Class Unzipper
  */
+
+class Uploader {
+
+  public function doUpload() {
+      $directory = "upload/";
+      if (strlen(pathinfo($_FILES['uploaded']['name'], PATHINFO_FILENAME)) == 0) {
+        $GLOBALS['status'] = array('error' => 'Fehler: Sie müssen eine Datei zum hochladen auswählen!');
+      }
+      else {
+        //checking if dir upload exists, if not create it.
+        if (!file_exists($directory)) {
+            mkdir($directory, 0755);
+        }
+        //moving the uploaded files to the upload-dir
+        $upload_folder = 'upload/'; //Das Upload-Verzeichnis
+        $filename = pathinfo($_FILES['uploaded']['name'], PATHINFO_FILENAME);
+        $extension = strtolower(pathinfo($_FILES['uploaded']['name'], PATHINFO_EXTENSION));
+
+
+        //Überprüfung der Dateiendung
+        $allowed_extensions = array('rar', 'zip', 'gz');
+        if(!in_array($extension, $allowed_extensions)) {
+          die("<p><b><font color='red' face='arial'>Fehler: </font><font face='arial'>Ungueltige Dateiendung. Nur .rar, .zip und .gz sind erlaubt.</font></b></p>");
+        }
+
+        //Überprüfung der Dateigröße
+        $max_size = 10000*1024; //10 MB
+        if($_FILES['uploaded']['size'] > $max_size) {
+          die("<p><b><font color='red' face='arial'>Fehler: </font> <font face='arial'>Bitte keine Dateien größer 10 MB hochladen.</font></b></p>");
+        }
+
+        //Pfad zum Upload
+        $new_path = $upload_folder.$filename.'.'.$extension;
+
+        //Neuer Dateiname falls die Datei bereits existiert
+        if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
+          $id = 1;
+          do {
+            $new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
+            $id++;
+          } while(file_exists($new_path));
+        }
+
+        //Alles okay, verschiebe Datei an neuen Pfad
+        move_uploaded_file($_FILES['uploaded']['tmp_name'], $new_path);
+        $GLOBALS['status'] = array('success' => 'Datei erfolgreich hochgeladen.');
+      }
+    }
+}
+
+
+
 class Unzipper {
   public $localdir = '.';
   public $zipfiles = array();
