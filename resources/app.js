fbcf8331e3f89c0f2056c06863d943a9018a69b3
Keinbockwurst <sboguth@web.de>
2016-11-04 14:59:17 +0100

    adding error message
    adding error message when file > 10MB before doing the upload

--- resources/app.js
+++ resources/app.js
@@ -4,4 +4,22 @@ $( document ).ready(function() {
     $(this).parent().find(".innercont").toggleClass("hide");
     $(this).parent().find(".icon").toggleClass("icon-up");
   });
+  $( "#uploadclick" ).click(function(e) {
+    var input, file;
+
+      if (!window.FileReader) {
+          alert("The file API isn't supported on this browser yet.");
+          return;
+      }
+
+      input = document.getElementById('fileinput');
+      file = input.files[0];
+      if (file.size > 1024 * 1024 * 10) {
+          alert("Die Datei ist größer als 10MB!");
+          e.preventDefault();
+      }
+      else {
+        //
+      }
+    });
 });
