fe0f9249a0285eb23bd76ac1ad0fbce82b9a4e71
Keinbockwurst <sboguth@web.de>
2016-11-09 15:00:27 +0100

    upload_max_filesize check before upload
    getting the upload_max_filesize for a check before upload

--- resources/app.js
+++ resources/app.js
@@ -1,25 +1,24 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   $('.file:even').addClass('even');
-  $( ".field h1, .icon" ).click(function() {
+  $(".field h1, .icon").click(function() {
     $(this).parent().find(".innercont").toggleClass("hide");
     $(this).parent().find(".icon").toggleClass("icon-up");
   });
-  $( "#uploadclick" ).click(function(e) {
+  $("#uploadclick").click(function(e) {
     var input, file;
 
-      if (!window.FileReader) {
-          alert("The file API isn't supported on this browser yet.");
-          return;
-      }
-
-      input = document.getElementById('fileinput');
-      file = input.files[0];
-      if (file.size > 1024 * 1024 * 10) {
-          alert("Die Datei ist größer als 10MB!");
-          e.preventDefault();
-      }
-      else {
-        //
-      }
-    });
+    if (!window.FileReader) {
+      alert("The file API isn't supported on this browser yet.");
+      return;
+    }
+     var filesize = parseInt($("#maxsize").text().slice(0,-3));
+    input = document.getElementById('fileinput');
+    file = input.files[0];
+    if (file.size > 1024 * 1024 * $('#maxsize').text().slice(0,-3)) {
+      alert("Die Datei ist größer als " + filesize + " MB!");
+      e.preventDefault();
+    } else {
+      //nuttin
+    }
+  });
 });
